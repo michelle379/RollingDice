9e7619ec6ebf458c5c330415762149909703340c
Michelle379 <michelle379123@gmail.com>
2024-10-14 19:46:42 +0800

    Initial Commit

---
+++ RollingDice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RollingDice
+//
+//  Created by 王宜婕 on 2024/10/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
